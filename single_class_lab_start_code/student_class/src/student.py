524e8a6859e05552bdb6b31997f4c056fbef2f7b
Jack <jackowendickson@gmail.com>
2022-02-28 13:55:14 +0000

    set up student.py

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,10 @@
-
+class Student:
+    def __init__(self, input_name, input_cohort, input_fav_language):
+        self.name = input_name
+        self.cohort = input_cohort
+        self.fav_language = input_fav_language
+#Creating methods
+    def talk(self):
+        print("I can talk!")
+    def say_favourite_language(self, fav_language):
+        print(f'I love {fav_language}!')
